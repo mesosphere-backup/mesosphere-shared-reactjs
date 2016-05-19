1d55149424001f7af5e5eb99d9b82e497367cb77
John Furrow <johnf@mesosphere.io>
2016-05-19 15:30:47 -0700

    Add #debounceOnError, #getErrorFromXHR, and #stubRequest to RequestUtil

--- utils/RequestUtil.js
+++ utils/RequestUtil.js
@@ -1,6 +1,8 @@
 var Reqwest = require("./Reqwest");
 var Util = require("./Util");
 
+var DEFAULT_ERROR_MESSAGE = "An error has occurred.";
+
 var activeRequests = {};
 
 function createCallbackWrapper(callback, requestID) {
@@ -23,8 +25,57 @@ function setRequestState(requestID, state) {
 }
 
 var RequestUtil = {
+  debounceOnError: function (interval, promiseFn, options) {
+    var rejectionCount = 0;
+    var timeUntilNextCall = 0;
+    var currentInterval = interval;
+    options = options || {};
+
+    if (typeof options.delayAfterCount !== "number") {
+      options.delayAfterCount = 0;
+    }
+
+    function resolveFn() {
+      rejectionCount = 0;
+      timeUntilNextCall = 0;
+      currentInterval = interval;
+    }
+
+    function rejectFn() {
+      rejectionCount++;
+
+      // Only delay if after delayAfterCount requests have failed
+      if (rejectionCount >= options.delayAfterCount) {
+        // Exponentially increase the time till the next call
+        currentInterval *= 2;
+        timeUntilNextCall = Date.now() + currentInterval;
+      }
+    }
+
+    var callback = promiseFn(resolveFn, rejectFn);
+
+    return function () {
+      if (Date.now() < timeUntilNextCall) {
+        return;
+      }
+
+      /* eslint-disable consistent-return */
+      return callback.apply(options.context, arguments);
+      /* eslint-enable consistent-return */
+    };
+  },
+
+  getErrorFromXHR: function (xhr) {
+    var response = this.parseResponseBody(xhr);
+    return response.description || response.message || DEFAULT_ERROR_MESSAGE;
+  },
+
   json: function (options) {
     if (options) {
+      if (options.url && !/\?/.test(options.url)) {
+        options.url += "?_timestamp=" + Date.now();
+      }
+
       if (Util.isFunction(options.hangingRequestCallback)) {
         var requestID = JSON.stringify(options);
         options.success = createCallbackWrapper(options.success, requestID);
@@ -76,10 +127,75 @@ var RequestUtil = {
     }
 
     if (responseText) {
-      return JSON.parse(responseText);
+      try {
+        return JSON.parse(responseText);
+      } catch (e) {
+        return {
+          description: responseText
+        };
+      }
     }
 
     return {};
+  },
+
+  /**
+   * Allows overriding the response of an ajax request as well
+   * as the success or failure of a request.
+   *
+   * @param  {Object} actionsHash The Actions file configuration
+   * @param  {String} actionsHashName The actual name of the actions file
+   * @param  {String} methodName The name of the method to be stubbed
+   * @return {Function} A function
+   */
+  stubRequest: function (actionsHash, actionsHashName, methodName) {
+    if (!global.actionTypes) {
+      global.actionTypes = {};
+    }
+
+    // Cache the method we're stubbing
+    var originalFunction = actionsHash[methodName];
+
+    function closure() {
+      // Store original RequestUtil.json
+      var requestUtilJSON = RequestUtil.json;
+      // `configuration` will contain the config that
+      // is passed to RequestUtil.json
+      var configuration = null;
+      // The configuration for the given method, this should be set externally
+      var methodConfig = global.actionTypes[actionsHashName][methodName];
+
+      // Proxy calls to RequestUtil.json
+      RequestUtil.json = function (object) {
+        configuration = object;
+      };
+
+      // The event type that we'll call success/error for the ajax request
+      var eventType = methodConfig.event;
+      // This will cause `configuration` to be set
+      originalFunction.apply(actionsHash, arguments);
+
+      // Restore RequestUtil.json
+      RequestUtil.json = requestUtilJSON;
+
+      var response = {};
+
+      if (methodConfig[eventType] && methodConfig[eventType].response) {
+        response = methodConfig[eventType].response;
+      } else if (eventType === "error") {
+        response = {responseJSON: {error: "Some generic error"}};
+      }
+
+      configuration[eventType](response);
+    }
+
+    // Return a function so that we can use setTimeout in the end.
+    return function () {
+      var args = arguments;
+      setTimeout(function () {
+        closure.apply(null, args);
+      }, global.actionTypes.requestTimeout || 500);
+    };
   }
 };
 
