19c2851c689ac271be4f963803029247b753e1f3
mlunoe <michael.lunoe@gmail.com>
2016-06-01 17:16:39 -0700

    Make ClassUtil take es6 classes as first argument

--- utils/ClassUtil.js
+++ utils/ClassUtil.js
@@ -1,31 +1,54 @@
-function es6ify(mixin) {
-  if (typeof mixin === "function") {
-    // mixin is already es6 style
-    return mixin;
+// Babel inherits function
+function inherits(subClass, superClass) {
+  if (typeof superClass !== 'function' && superClass !== null) {
+    throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass);
   }
 
-  return function (Base) {
-    // mixin is old-react style plain object
-    // convert to ES6 class
-    function MixinClass() {}
-    MixinClass.prototype = Object.create(Base.prototype);
-    MixinClass.prototype.constructor = MixinClass;
-    Object.assign(MixinClass.prototype, mixin);
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
 
-    return MixinClass;
-  };
+  if (superClass) {
+    if (Object.setPrototypeOf) {
+      Object.setPrototypeOf(subClass, superClass);
+    } else {
+      subClass.__proto__ = superClass;
+    }
+  }
 }
 
-function mixin() {
-  var mixins = Array.prototype.slice.call(arguments);
+function es6ify(Base, mixin) {
+  // mixin is old-react style plain object
+  // convert to ES6 class
+  function MixinClass() {}
+  MixinClass.prototype = Object.create(Base.prototype);
+  MixinClass.prototype.constructor = MixinClass;
+  Object.assign(MixinClass.prototype, mixin);
 
+  return MixinClass;
+}
+
+function mixin(Parent /*, ...mixins*/) {
+  var mixins = Array.prototype.slice.call(arguments, 1);
   // Creates base class
   function Base() {}
-
   mixins.reverse();
 
+  if (typeof Parent === 'function') {
+    // Make Base inherit from Parent, if Parent is a class
+    inherits(Base, Parent);
+  } else {
+    // Parent is a regular object, let's add it to mixins
+    mixins.push(Parent);
+  }
+
   mixins.forEach(function (mixin) {
-    Base = es6ify(mixin)(Base);
+    Base = es6ify(Base, mixin);
   });
 
   return Base;
