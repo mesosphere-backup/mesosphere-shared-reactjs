a4a9cd3e9ad7295fa77ced012e9c2faca3215f9c
mlunoe <michael.lunoe@gmail.com>
2016-09-12 13:21:55 -0700

    Remove unused GetSetMixin

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var ClassMixin = require("./utils/ClassUtil").mixin;
-var GetSetMixin = require("./mixins/GetSetMixin");
 var RequestUtil = require("./utils/RequestUtil");
 var StoreMixin = require("./mixins/StoreMixin");
 
 module.exports = {
   ClassMixin: ClassMixin,
-  GetSetMixin: GetSetMixin,
   StoreMixin: StoreMixin,
   RequestUtil: RequestUtil
 };
